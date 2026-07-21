5a3a5de21745b9e3883d43d45f300a9f474933b3
robz <maylow2077@gmail.com>
2026-07-21 11:46:17 -0300

    feat(auth): implementa registro e login com JWT

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { authRoutes } from './routes/auth'
 
 const app = Fastify()
 
 app.register(cors, { origin: 'http://localhost:5173' })
+
+app.register(authRoutes, { prefix: '/api' })
 
 app.listen({ port: Number(process.env.PORT) || 3035, host: '0.0.0.0' }, (err, address) => {
   if (err) throw err
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -9,7 +9,8 @@ const JWT_SECRET = process.env.JWT_SECRET || 'fallback_secret'
 
 export async function authRoutes(app: FastifyInstance): Promise<void> {
 
-  app.post('auth/register', async (request, reply) => {
+  //registro de usuário
+  app.post('/auth/register', async (request, reply) => {
     const { email, password, name } = request.body as {
       email: string;
       password: string;
@@ -50,5 +51,38 @@ export async function authRoutes(app: FastifyInstance): Promise<void> {
       JWT_SECRET, { expiresIn: '1h' })
 
     return { token, user: newUser }  
+  })
+  
+  //login de usuário
+  app.post('/auth/login', async (request, reply) => {
+    const { email, password} = request.body as {
+      email: string;
+      password: string;
+    }
+
+    //verifica se o usuário existe
+    const result = await db.select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1)
+    
+    
+    if (result.length === 0) {
+      return reply.status(401).send({ message: 'Email ou senha inválidos' })
+    }
+
+    const user = result[0]!
+
+    //verifica se a senha está correta
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (!validPassword) {
+      return reply.status(401).send({ message: 'Email ou senha inválidos' })
+    }
+
+    //gera o token JWT
+    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' })
+
+    return { token, user: { id: user.id, email: user.email, name: user.name } }
+
   })
 }
